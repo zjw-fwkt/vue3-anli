6a9a47ac9dd385f5b594df5d071daf675cdfc7be
zjw <760371303@qq.com>
2021-11-14 22:37:27 +0800

    初始仓

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,24 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+const routes = [{
+        path: "/",
+        name: "login",
+
+        component: () =>
+            import ("../views/acount/Login.vue"),
+    },
+    {
+        path: "/about",
+        name: "About",
+
+        component: () =>
+            import ("../views/About.vue"),
+    },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+    history: createWebHashHistory(),
+    routes,
 });
 
 export default router;
---
+++ src/views/acount/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="account">
+    登录
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped lang='scss'>
+@import "./style.scss";
+</style>
