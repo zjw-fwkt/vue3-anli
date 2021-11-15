8860070383d1aff8feb4799888814fc4d7701b76
zjw <760371303@qq.com>
2021-11-15 15:58:03 +0800

    初始仓库

--- src/views/acount/Login.vue
+++ src/views/acount/Login.vue
@@ -1,12 +1,55 @@
 <template>
   <div class="account">
-    登录
+    <div class="form-wrap">
+      <a-form :form="form"
+              ref="ruleForm">
+
+        <a-form-item>
+          <label>用户名</label>
+          <a-input type="text"
+                   autocomplete="off" />
+        </a-form-item>
+
+        <a-form-item>
+          <label>密码</label>
+          <a-input type="password"
+                   autocomplete="off" />
+        </a-form-item>
+
+        <a-form-item>
+          <a-button type="primary"
+                    html-type="submit"
+                    block>
+            登录
+          </a-button>
+        </a-form-item>
+      </a-form>
+      <dir class="fs-12 text-center">
+        <a href="" class="color-white">忘记密码</a> |
+        <a href="" class="color-white">注册</a>
+      </dir>
+    </div>
   </div>
 </template>
 <script>
+import { reactive } from 'vue'
 export default {
+  name: "Login",
+  setup () {
+
+    const formConfig = reactive({
+      layout: {
+        labelCol: { span: 4 },
+        wrapperCol: { span: 14 },
+      }
+    })
+
+    return {
+      formConfig
+    }
+  }
 }
 </script>
 <style scoped lang='scss'>
-@import "./style.scss";
+@import './style.scss';
 </style>
